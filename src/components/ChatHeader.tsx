4e01eb3ec5ccd92b2ae1a4d7973aa49b499e8bb7
Nguyen Nhat Truong <nhattruongnguyenag@gmail.com>
2023-09-09 09:47:29 +0700

    .

--- src/components/ChatHeader.tsx
+++ src/components/ChatHeader.tsx
@@ -8,8 +8,8 @@ interface ChatHeaderProps {
 export default function ChatHeader({ data }: ChatHeaderProps) {
   return (
     <div className='fixed bg-blue-500 ps-2 left-0 right-0 md:rounded-tl-xl md:rounded-tr-xl'>
-      <div className='flex flex-row items-center rounded-xl p-2'>
-        <div className='flex items-center justify-center h-14 w-14 bg-indigo-200 rounded-full'>{data?.fullName[0]}</div>
+      <div className='flex flex-row items-center rounded-xl px-2 py-3'>
+        <div className='flex items-center justify-center h-12 w-12 bg-indigo-200 rounded-full'>{data?.fullName[0]}</div>
         <div className='ml-2 text-lg font-semibold text-white'>{data?.fullName}</div>
       </div>
     </div>
