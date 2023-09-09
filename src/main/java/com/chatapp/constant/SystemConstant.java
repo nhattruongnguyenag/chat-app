f830c8d87ee37df1dd5dd4bca93ec38df0f76242
Nguyen Nhat Truong <nhattruongnguyenag@gmail.com>
2023-09-09 08:34:45 +0700

    update cors orgin

--- src/main/java/com/chatapp/constant/SystemConstant.java
+++ src/main/java/com/chatapp/constant/SystemConstant.java
@@ -1,5 +1,5 @@
 package com.chatapp.constant;
 
 public class SystemConstant {
-    public static final String ACCESS_CONTROL_ALLOW_ORIGIN = "http://localhost:3000";
+    public static final String ACCESS_CONTROL_ALLOW_ORIGIN = "http://nhattruongnguyen.com";
 }
